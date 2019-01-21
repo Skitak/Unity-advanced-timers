89b46a55b90f2ecf58694227d828fd42105a16e1
skitak <bas2205@live.fr>
2019-01-21 23:50:10 +0100

    timers added

---
+++ TimerManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerManager : MonoBehaviour {
+	private static ArrayList timers = new ArrayList (); 
+
+	void Update () {
+		foreach (Timer t in timers){
+			t.Update(Time.deltaTime);
+		}
+	}
+		
+	public static void SetupTimer(Timer t){
+		timers.Add (t);
+	}
+}
