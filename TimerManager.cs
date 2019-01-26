489e99380c1c92fd953f3549069f03b26c795d13
skitak <bas2205@live.fr>
2019-01-26 10:04:43 +0100

    global pause added

--- TimerManager.cs
+++ TimerManager.cs
@@ -3,8 +3,10 @@ using System.Collections;
 
 public class TimerManager : MonoBehaviour {
 	private static ArrayList timers = new ArrayList (); 
-
+	public static bool isPlaying = true; 
 	void Update () {
+		if (!isPlaying)
+			return;
 		foreach (Timer t in timers){
 			t.Update(Time.deltaTime);
 		}
@@ -12,5 +14,9 @@ public class TimerManager : MonoBehaviour {
 		
 	public static void SetupTimer(Timer t){
 		timers.Add (t);
+	}
+
+	public void Pause() {
+
 	}
 }
